810dd619532b9fae98809e61a8590c699f447d8f
VitalieBraga <vitaliebraga@gmail.com>
2017-09-20 13:05:04 -0700

    created the all compoments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule } from '@angular/core';import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductService } from './product/product.service';
+import { ProductListComponent } from './product-list/product-list.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { NewProductComponent } from './new-product/new-product.component'; // <-- Import ProductService
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    EditProductComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // <-- Include module in our AppModules
+		HttpModule // <-- Include module in our AppModules
   ],
-  providers: [],
+  providers: [ProductService], // <-- Include service in our Providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
